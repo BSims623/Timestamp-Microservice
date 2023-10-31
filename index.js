d83bb7af4732f7e75d301328301b93bdd1ba69ff
Brian Sims <briansims623@gmail.com>
2023-10-30 23:21:10 -0700

    Initial Commit

--- index.js
+++ index.js
@@ -5,10 +5,11 @@
 var express = require('express');
 var app = express();
 
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -21,8 +22,22 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
+
+app.get("/api/:id", (req, res) => {
+  console.log(req.params.id)
+  const id = req.params.id;
+  const utc = new Date(id).toUTCString();
+  const unix = new Date(id).getTime();
+
+  if (id.match(/^-\d+$|^\d+$/)) {
+    return res.json({ "unix": id, "utc": new Date(Number(req.params.id)).toUTCString() })
+  } else if (utc === "Invalid Date") {
+    return res.json({ "error": utc })
+  }
+  res.json({ "unix": new Date(id).getTime(), "utc": new Date(id).toUTCString() })
+})
 
 
 
